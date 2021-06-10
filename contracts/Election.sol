add33604a0e1397bd7de378dcf6e91453c6a2df2
Trend20 <enockomondi305@gmail.com>
2021-06-10 14:28:43 +0300

    creating a function to add candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,6 +2,31 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Election {
+
+  // declare candidate variable
+  // string public candidate;
+
+  // To store more than one candidate details we use STRUCT
+  struct Candidate{
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  // read and write more than one candidate
+  mapping(uint =>Candidate) public candidates;
+
+  // strore candidates count in the contract
+  uint public candidatesCount;
+
+  // FUNTION TO ADD CANDIDATES
+  function addCandidate(string _name) private{
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  // initialize a constructor
   constructor() public {
+    candidate = "Candidate1";
   }
 }
